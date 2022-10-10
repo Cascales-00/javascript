0c6d4951d6408bb3af9a9001c233cb589b9c8ed7
Cascales-00 <58515219+Cascales-00@users.noreply.github.com>
2022-10-10 11:32:59 -0300

    update ex013

---
+++ revisao 2/nivel1-js-basico/js/script010.js
@@ -0,0 +1,17 @@
+let res = document.querySelector('section#resultado')
+
+function bt1 () {
+    res.innerHTML += `<p>Clicou no primeiro botão</p>`
+}
+
+function bt2 () {
+    res.innerHTML += `<p>Clicou no segundo botão</p>`
+}
+
+function bt3 () {
+    res.innerHTML += `<p>Clicou no terceiro botão</p>`
+}
+
+function bt4 () {
+    res.innerHTML += `<p>Clicou no quarto botão</p>`
+}
